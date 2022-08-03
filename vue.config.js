8c2008103ccaf9f69f13fe09a91913f47c359a0b
DariaBurakova <DariaBurakova@gmail.com>
2022-08-03 16:30:20 +0300

    first commit

---
+++ vue.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: [
+    'vuetify'
+  ]
+})
+module.exports= {
+  devServer:{
+    port:8047,
+  }
+}
